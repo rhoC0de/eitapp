640ea571096cd1cdb0318dfa35e41a3fabfb4f1a
rhoCode <oreoluwaakanni@outlook.com>
2018-09-17 19:51:04 +0000

    change task to eit and text to eit info

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,9 +6,9 @@ import './body.html';
 
 Template.body.helpers({
   tasks: [
-    { name: 'Akanni Oreoluwa' },
-    { name: 'Amanda Williams' },
-    { name: 'Solomon Igori' },
+    { firstname: 'Oreoluwa', surname: 'Akanni', gender: 'F', dob: '9th March' },
+    { firstname: 'Amanda', surname: 'Williams', gender: 'F', dob: '16th June' },
+    { firstname: 'Solomon', surname: 'Igori', gender: 'M', dob: '21st Feburary' },
   ],
 });
 
