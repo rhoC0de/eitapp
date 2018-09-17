b418011a25b1c4a4f6c65d6fba22459fca5aed7c
rhoCode <oreoluwaakanni@outlook.com>
2018-09-17 21:17:56 +0000

    Add event handler for form submit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -10,3 +10,31 @@ Template.body.helpers({
   },
 });
 
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const firstname = target.firstname.value;
+    const surname = target.surname.value;
+    const gender = target.gender.value;
+    const dob = target.dob.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      firstname,
+      surname,
+      gender,
+      dob,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.firstname.value = '';
+    target.surname.value = '';
+    target.gender.value = '';
+    target.dob.value = '';
+  },
+});
